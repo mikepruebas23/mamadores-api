6fbe4cb756a931078124dbd5a8de4272a55ec2be
Miguel Armenta <mikepruebas23@gmail.com>
2021-07-27 21:22:33 -0600

    agregando el post

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function getData() {
         (mamador) => {
             let newDiv = document.createElement("div");
             let newComent = document.createElement("div");
-            newDiv.innerHTML = '@'+mamador.name;
+            newDiv.innerHTML = '@' + mamador.name;
             newComent.innerHTML = mamador.description;
             stock.appendChild(newDiv);
             frase.appendChild(newComent);
@@ -20,3 +20,33 @@ function getData() {
 
 
 getData();
+
+
+var urlPost = 'https://mamadores-api.herokuapp.com/mamador';
+//     fetch(url, {
+//             method: 'POST',
+//             body: JSON.stringify({
+//                 name: "ElToxico",
+//                 description: "No sé donde estas, ni si volveras.",
+//                 image: 'No',
+//                 category: 'Relaciones'
+//             }),
+//             headers: {
+//                 "Content-type": "application/json"
+//             }}).then(response => response.json()).then(json => console.log(json))
+
+const thisForm = document.getElementById('myForm');
+thisForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const formData = new FormData(thisForm).entries()
+    const response = await fetch(urlPost, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formData))
+    });
+
+    const result = await response.json();
+    console.log(result)
+});
