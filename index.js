c5ac1ac1982a077a132e4762f6f7bae381637bbb
Miguel Armenta <mikepruebas23@gmail.com>
2021-07-28 14:56:01 -0600

    background

--- index.js
+++ index.js
@@ -1,52 +1,78 @@
-var url = 'https://mamadores-api.herokuapp.com/mamador/random';
-var stock = document.getElementById('username');
-var frase = document.getElementById('comment');
-var contentImg = document.getElementById('content-img');
-var postCategory = document.getElementById('postCategory');
+const URL_API_CREATE_NEW_POST = 'https://mamadores-api.herokuapp.com/mamador';
+const URL_API_GET_ONE_RANDOM_POST = 'https://mamadores-api.herokuapp.com/mamador/random';
 
-function getData() {
+const stock = document.getElementById('username');
+const frase = document.getElementById('comment');
+const contentImg = document.getElementById('content-img');
+const postCategory = document.getElementById('postCategory');
+
+function getRandomPost() {
+
+    
+
     while (stock.firstChild) stock.removeChild(stock.firstChild);
     while (frase.firstChild) frase.removeChild(frase.firstChild);
     while (postCategory.firstChild) postCategory.removeChild(postCategory.firstChild);
-    fetch(url).then(response => response.json()).then(
-        (mamador) => {
-            let newDiv = document.createElement("div");
-            let newComent = document.createElement("div");
-            let newCategory = document.createElement("div");
 
-            newDiv.innerHTML = '@' + mamador.name;
-            newComent.innerHTML = mamador.description;
-            newCategory.innerHTML = " "+mamador.category+" ";
+    // fetch(URL_API_GET_ONE_RANDOM_POST).then(response => response.json()).then(
+    //     (res) => {
+    //         let newDiv = document.createElement("div");
+    //         let newComent = document.createElement("div");
+    //         let newCategory = document.createElement("div");
 
-            stock.appendChild(newDiv);
-            frase.appendChild(newComent);
-            postCategory.appendChild(newCategory);
+    //         newDiv.innerHTML = '@' + res.name;
+    //         newComent.innerHTML = res.description;
+    //         newCategory.innerHTML = " " + res.category + " ";
+
+    //         stock.appendChild(newDiv);
+    //         frase.appendChild(newComent);
+    //         postCategory.appendChild(newCategory);
+    //     }
+    // );
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = 'Cargando Publicación...';
+    stock.appendChild(newDiv);
+
+    let processStatus = function (response) {
+        if (response.status === 200) {
+            return Promise.resolve(response);
+        } else {
+            return Promise.reject(new Error(response.statusText))
         }
-    );
+    };
+
+    fetch(URL_API_GET_ONE_RANDOM_POST)
+    .then(response => response.json())
+    .then((res) => {
+
+        while (stock.firstChild) stock.removeChild(stock.firstChild);
+
+        let newDiv = document.createElement("div");
+        let newComent = document.createElement("div");
+        let newCategory = document.createElement("div");
+
+        newDiv.innerHTML = '@' + res.name;
+        newComent.innerHTML = res.description;
+        newCategory.innerHTML = " " + res.category + " ";
+
+        stock.appendChild(newDiv);
+        frase.appendChild(newComent);
+        postCategory.appendChild(newCategory);
+    })
+    .catch(function(){
+        Swal.fire('Error al cargar la publicación.');
+    });
+
+    
+
 }
 
-
-getData();
-
-
-var urlPost = 'https://mamadores-api.herokuapp.com/mamador';
-//     fetch(url, {
-//             method: 'POST',
-//             body: JSON.stringify({
-//                 name: "ElToxico",
-//                 description: "No sé donde estas, ni si volveras.",
-//                 image: 'No',
-//                 category: 'Relaciones'
-//             }),
-//             headers: {
-//                 "Content-type": "application/json"
-//             }}).then(response => response.json()).then(json => console.log(json))
 
 const thisForm = document.getElementById('myForm');
 thisForm.addEventListener('submit', async function (e) {
     e.preventDefault();
     const formData = new FormData(thisForm).entries()
-    const response = await fetch(urlPost, {
+    const response = await fetch(URL_API_CREATE_NEW_POST, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -55,10 +81,12 @@ thisForm.addEventListener('submit', async function (e) {
     });
 
     const result = await response.json();
+
     document.getElementById('name').value = '';
     document.getElementById('description').value = '';
     document.getElementById('category').value = '';
-    // document.getElementById('myform').reset();
-    // console.log(result.message);
+
     Swal.fire(result.message);
 });
+
+getRandomPost();
