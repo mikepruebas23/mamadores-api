6ca1ef7ed2b988d9fac2d7a31a99491d49863fb8
Miguel Armenta <mikepruebas23@gmail.com>
2021-07-28 22:27:35 -0600

    añadiendo las imagenes

--- index.js
+++ index.js
@@ -1,60 +1,40 @@
 const URL_API_CREATE_NEW_POST = 'https://mamadores-api.herokuapp.com/mamador';
 const URL_API_GET_ONE_RANDOM_POST = 'https://mamadores-api.herokuapp.com/mamador/random';
+const URL_API_GET_IMAGE = 'https://mamadores-api.herokuapp.com/';
 
 const stock = document.getElementById('username');
 const frase = document.getElementById('comment');
 const contentImg = document.getElementById('content-img');
 const postCategory = document.getElementById('postCategory');
+const postSubmit = document.getElementById("postSubmit");
+const imgPost = document.getElementById('cImg');
 
 function getRandomPost() {
-
-
 
     while (stock.firstChild) stock.removeChild(stock.firstChild);
     while (frase.firstChild) frase.removeChild(frase.firstChild);
     while (postCategory.firstChild) postCategory.removeChild(postCategory.firstChild);
 
-    // fetch(URL_API_GET_ONE_RANDOM_POST).then(response => response.json()).then(
-    //     (res) => {
-    //         let newDiv = document.createElement("div");
-    //         let newComent = document.createElement("div");
-    //         let newCategory = document.createElement("div");
-
-    //         newDiv.innerHTML = '@' + res.name;
-    //         newComent.innerHTML = res.description;
-    //         newCategory.innerHTML = " " + res.category + " ";
-
-    //         stock.appendChild(newDiv);
-    //         frase.appendChild(newComent);
-    //         postCategory.appendChild(newCategory);
-    //     }
-    // );
     let newDiv = document.createElement("div");
     newDiv.innerHTML = 'Cargando Publicación...';
     stock.appendChild(newDiv);
-
-    let processStatus = function (response) {
-        if (response.status === 200) {
-            return Promise.resolve(response);
-        } else {
-            return Promise.reject(new Error(response.statusText))
-        }
-    };
 
     fetch(URL_API_GET_ONE_RANDOM_POST)
         .then(response => response.json())
         .then((res) => {
 
-            // console.log("RES: ",res.image);
-            // if(res.image != '0'){
-
-            // }
+            
+            
 
             while (stock.firstChild) stock.removeChild(stock.firstChild);
 
             let newDiv = document.createElement("div");
             let newComent = document.createElement("div");
             let newCategory = document.createElement("div");
+            // let newImg = document.createElement("img");
+
+            // newImg.classList.add("card-img");
+            // newImg.src = URL_API_GET_IMAGE + res.image; 
 
             newDiv.innerHTML = '@' + res.name;
             newComent.innerHTML = res.description;
@@ -63,19 +43,33 @@ function getRandomPost() {
             stock.appendChild(newDiv);
             frase.appendChild(newComent);
             postCategory.appendChild(newCategory);
+            // imgPost.appendChild(newImg); 
+
+            if(res.image != '0' && res.image != 'No'){
+                while (imgPost.firstChild) imgPost.removeChild(imgPost.firstChild);
+                console.log("RES: ",URL_API_GET_IMAGE+res.image);
+                let newImg = document.createElement("img");
+                newImg.src = URL_API_GET_IMAGE + res.image;
+                newImg.classList.add("card-img");
+                imgPost.appendChild(newImg); 
+            }
+            else {
+                while (imgPost.firstChild) imgPost.removeChild(imgPost.firstChild);
+            }
+
+            // <!-- <img class="img-profile" src="https://picsum.photos/200/300" alt="user-img"> -->
         })
         .catch(function () {
             Swal.fire('Error al cargar la publicación.');
         });
-
-
-
 }
 
 
 const thisForm = document.getElementById('myForm');
 thisForm.addEventListener('submit', async function (e) {
 
+    postSubmit.classList.add("btn-blocked");
+    postSubmit.disabled = true;
 
     e.preventDefault();
 
@@ -96,14 +90,22 @@ thisForm.addEventListener('submit', async function (e) {
         redirect: 'follow'
     };
 
-    const response = await fetch(URL_API_CREATE_NEW_POST, requestOptions);
-    const result = await response.json();
+    const response = await fetch(URL_API_CREATE_NEW_POST, requestOptions).then(() =>{
 
-    document.getElementById('name').value = '';
-    document.getElementById('description').value = '';
-    document.getElementById('category').value = '';
+        const result = response.json();
 
-    Swal.fire(result.message);
+        document.getElementById('name').value = '';
+        document.getElementById('description').value = '';
+        document.getElementById('category').value = '';
+
+        postSubmit.classList.remove("btn-blocked");
+        postSubmit.disabled = false;
+        Swal.fire(result.message);
+    })
+    .catch(function () {
+        Swal.fire('Error al Publicar..');
+    });
+    
 });
 
 getRandomPost();
